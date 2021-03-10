8bfab0fcc7abba7430b1b38fbf16347fa591d863
businan <yorulmazburaksinan@gmail.com>
2021-03-11 00:39:03 +0100

    added get method also added routes and controllers

--- api/routes.js
+++ api/routes.js
@@ -5,7 +5,15 @@ const router = express.Router();
 
 router.get('/', controllers.hello);
 
-// write your routes
+router.get("/courses", controllers.getCourses);
+
+router.get("/courses/:id", controllers.getSingleCourse);
+
+router.put("/courses/:id", controllers.putCourse);
+
+router.delete("/courses/:id", controllers.deleteCourse);
+
+router.post("/courses", controllers.postCourse);
 
 
 module.exports = router;
